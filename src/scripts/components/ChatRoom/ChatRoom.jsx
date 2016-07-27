b7c6df9f5dc2533c1da08915c3ddfd75fecaeb37
Gabriel Pages <gbpages@yahoo.com>
2016-07-27 01:36:00 -0400

    sending and recieving messages in console

--- src/scripts/components/ChatRoom/ChatRoom.jsx
+++ src/scripts/components/ChatRoom/ChatRoom.jsx
@@ -6,24 +6,32 @@ import MainSection from "./MainSection/MainSection.jsx"
 
 
 export default class ChatRoom extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pubnub: undefined,
+      messages: []
+    };
+  }
 componentWillMount(){
 
   const uuid = this.props.uuid;
-  console.log(uuid);
 
   // Initialize PubNub instance with personal UUID from login page
   const pubnub = PUBNUB({
-    subscribe_key: 'pub-c-3dd6d969-eaef-4b1d-a3df-796e4240ec1e',
-    publish_key: 'sub-c-01fea1f4-4d1b-11e6-9c7c-0619f8945a4f',
+    subscribe_key: 'sub-c-426f2002-53b9-11e6-a5a4-0619f8945a4f',
+    publish_key: 'pub-c-a5512859-880e-471c-91ff-365d2bf1b694',
     uuid: uuid
   });
 
+
+  this.setState({pubnub: pubnub});
 }
  render() {
   return (
     <div id='container'>
-      <Sidebar />
-      <MainSection />
+      <Sidebar pubnub={this.state.pubnub} />
+      <MainSection pubnub={this.state.pubnub} />
     </div>
   );
  }
--- src/scripts/components/ChatRoom/MainSection/InputSection.jsx
+++ src/scripts/components/ChatRoom/MainSection/InputSection.jsx
@@ -2,10 +2,32 @@ import React from "react";
 
 
 export default class InputSection extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {pubnub: this.props.pubnub};
+  }
+  sendMessage(e){
+    e.preventDefault()
+    const pubnub = this.state.pubnub;
+
+    let message = document.getElementById('text-input').value;
+    document.getElementById('text-input').value = null;
+
+    pubnub.publish({
+      channel : 'suncoast-chat',
+      message : message,
+      callback: function(m){
+          console.log("callback from publish: ", m);
+      },
+      error: function(e){
+        console.log(e);
+      }
+    });
+  }
  render() {
   return (
     <div className="input-section">
-      <form action="">
+      <form onSubmit={this.sendMessage.bind(this)}>
         <input id="text-input" type="text" placeholder="Hello World." />
         <input id="submit-button" type="submit" />
       </form>

0c0f0776c2ba68fa71a44b135e302b6e45a83d4e
Gabriel Pages <gbpages@yahoo.com>
2016-07-27 00:17:51 -0400

    added step 1

--- src/scripts/components/ChatRoom/ChatRoom.jsx
+++ src/scripts/components/ChatRoom/ChatRoom.jsx
@@ -6,7 +6,19 @@ import MainSection from "./MainSection/MainSection.jsx"
 
 
 export default class ChatRoom extends React.Component {
+componentWillMount(){
 
+  const uuid = this.props.uuid;
+  console.log(uuid);
+
+  // Initialize PubNub instance with personal UUID from login page
+  const pubnub = PUBNUB({
+    subscribe_key: 'pub-c-3dd6d969-eaef-4b1d-a3df-796e4240ec1e',
+    publish_key: 'sub-c-01fea1f4-4d1b-11e6-9c7c-0619f8945a4f',
+    uuid: uuid
+  });
+
+}
  render() {
   return (
     <div id='container'>
